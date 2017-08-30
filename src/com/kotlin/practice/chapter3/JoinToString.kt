ad1287aa5e8908a11628ee4c033234480809719e
ramnivasindani <ram.0737@gmail.com>
2017-08-30 15:00:19 -0700

    updated function

--- src/com/kotlin/practice/chapter3/JoinToString.kt
+++ src/com/kotlin/practice/chapter3/JoinToString.kt
@@ -1,5 +1,26 @@
 package com.kotlin.practice.chapter3
 
+import java.util.*
+
 /**
  * Created by ramnivasindani on 8/28/17.
  */
+
+fun <T> joinToString(collection: Collection<T>,
+                 seperator: String,
+                 prefix: String,
+                 postfix: String) : String {
+    var builder = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) {
+            builder.append(seperator)
+        }
+        builder.append(element)
+    }
+    builder.append(postfix)
+    return builder.toString()
+}
+
+fun main(args: Array<String>) {
+    println(joinToString(listOf(1,2,3,4), seperator="|", prefix = "{", postfix = "}"))
+}
